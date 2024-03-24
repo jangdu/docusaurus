3967fa41dc148bc4863b5909121581104ba7f6ae
jangdu <jjd0324@gmail.com>
2024-03-25 00:12:25 +0900

    fix: build err

--- src/gtm.js
+++ src/gtm.js
@@ -1,14 +1,18 @@
 // src/gtm.js
-document.addEventListener("DOMContentLoaded", function () {
-  var noscript = document.createElement("noscript");
-  var iframe = document.createElement("iframe");
+if (typeof window !== "undefined") {
+  // document를 사용하는 코드 여기에 작성
 
-  iframe.src = "https://www.googletagmanager.com/ns.html?id=GTM-T9SX9HQG";
-  iframe.height = "0";
-  iframe.width = "0";
-  iframe.style.display = "none";
-  iframe.style.visibility = "hidden";
+  document.addEventListener("DOMContentLoaded", function () {
+    var noscript = document.createElement("noscript");
+    var iframe = document.createElement("iframe");
 
-  noscript.appendChild(iframe);
-  document.body.insertBefore(noscript, document.body.firstChild);
-});
+    iframe.src = "https://www.googletagmanager.com/ns.html?id=GTM-T9SX9HQG";
+    iframe.height = "0";
+    iframe.width = "0";
+    iframe.style.display = "none";
+    iframe.style.visibility = "hidden";
+
+    noscript.appendChild(iframe);
+    document.body.insertBefore(noscript, document.body.firstChild);
+  });
+}

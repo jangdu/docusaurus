c71581718be7d8569229d0adf21660ff5564e064
jangdu <jjd0324@gmail.com>
2024-03-24 21:38:58 +0900

    docs: add seo config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -80,6 +80,24 @@ const config: Config = {
 
         gtag('config', 'G-3ZK2V0X0EH');
       </script>
+
+
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-T9SX9HQG');</script>
+      <!-- End Google Tag Manager -->
+      `,
+    },
+
+    bodyTags: {
+      innerHtml: `
+      <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T9SX9HQG"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <!-- End Google Tag Manager (noscript) -->
       `,
     },
 
